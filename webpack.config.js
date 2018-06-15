d69ee26f9d0df6bd2b38d6841386dde1c2e41485
Anna Bagrowska <anna_bagrowska@wp.pl>
2018-06-15 18:06:52 +0200

    add optimalize-js-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const plugins = [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+})];
 
 module.exports = (env) => {
-    return {
+        if (env === 'production') {
+            plugins.push(
+                new OptimizeJsPlugin({
+                    sourceMap: false
+                })
+            )
+        }
+        return {
         mode: env || 'production',
         entry: './src/index.js',
             output: {
@@ -31,12 +45,6 @@ module.exports = (env) => {
                 }
             ]           
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: 'src/index.html',
-                filename: 'index.html',
-                inject: 'body'
-            })
-        ]
+        plugins
     }
 };
